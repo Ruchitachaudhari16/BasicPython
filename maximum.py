b5f55e4e9cd4e63958cc7e5383912d43de6661ab
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-06-10 16:46:23 +0530

    [Ruchita] Add: python script for maximum number program

---
+++ maximum.py
@@ -0,0 +1,13 @@
+def maximumno():
+    numberone = 10
+    numbertwo = 200
+    if numberone > numbertwo:
+        print("Number1 is greater: " + str(numberone))
+    else:
+        print("Number2 is greater: " + str(numbertwo))
+
+def main():
+    maximumno()
+
+if __name__ == "__main__":
+    main()
