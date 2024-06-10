4695dc9e8e3deb94c8fa21506caa1198a9334359
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-06-10 16:52:28 +0530

    [Ruchita] Add: python script for whileloop python program

---
+++ whileloop.py
@@ -0,0 +1,12 @@
+def whileloop():
+    count=0
+    while (count<5):
+        count=count+1
+        print("Ruchita")
+        
+def main():
+    whileloop()
+    
+if __name__=="__main__":
+  main()
+    
