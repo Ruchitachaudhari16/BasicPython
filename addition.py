eb12116d26cc029dd02a57c6a8e27a2c3e81c7ce
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-06-10 16:41:34 +0530

    [Ruchita] Add: python script to calculate addition of number program

---
+++ addition.py
@@ -0,0 +1,38 @@
+# addition,subtraction,multiplication,Divisiom function
+def addition():
+    x=10
+    y=100
+    c=x+y
+    print(c)
+    
+addition()
+
+def subtraction():
+    a=1000
+    b=500
+    c=a-b
+    print(c)
+
+subtraction()
+
+def multiplication():
+    a=200
+    b=50
+    c=a*b
+    print(c)
+
+multiplication()
+
+def division():
+    x=100
+    y=20
+    z=x/y
+    print(z)
+
+
+def main():
+  division()
+  
+  
+if __name__ == "__main__":
+    main()
