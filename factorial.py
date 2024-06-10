2b3afdd2bf63057f87dda758fa030e3a6bfc25bb
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-06-10 16:43:33 +0530

    [Ruchita] Add: python script for factorial program

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial():
+    number = 3
+    fact = 1
+    for i in range(1, number + 1):
+        fact *= i
+    print(fact)
+
+def main():
+  factorial()
+  
+  if__name__="__main__"
+main()
