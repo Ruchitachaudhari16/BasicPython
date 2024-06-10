08cef393ef127812f8113e7dc2f58279d69cf089
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-06-10 16:44:30 +0530

    [Ruchita] Add: python script to take user input program

---
+++ inputuser.py
@@ -0,0 +1,12 @@
+numberone = input("Enter number one: ")
+numbertwo = input("Enter number two: ")
+
+# Convert the inputs to integers
+numberone = int(numberone)
+numbertwo = int(numbertwo)
+
+# Calculate the sum
+sum = numberone + numbertwo
+
+# Print the result
+print("Sum of two numbers:", sum)
