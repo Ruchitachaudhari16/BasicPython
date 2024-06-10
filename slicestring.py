3ad459bce13bebb98dc3f482fa9543fcaee6d58f
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-06-10 16:50:47 +0530

    [Ruchita] Add: python script for slicing function python program

---
+++ slicestring.py
@@ -0,0 +1,19 @@
+# String slicing
+String = 'RuchitaforGeeks'
+
+slice_obj1 = slice(3)
+slice_obj2 = slice(1, 5, 3)
+
+print("String slicing")
+print(String[slice_obj1])
+print(String[slice_obj2])
+
+
+
+L = [1, 2, 3, 4, 5]
+slice_obj1 = slice(3)
+slice_obj2 = slice(1, 5, 2)
+
+print("List slicing")
+print(L[slice_obj1])
+print(L[slice_obj2])
